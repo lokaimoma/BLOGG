f1dcb8feed09fc3d875130d1822ba37bbd55e2df
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-06-28 19:51:20 +0100

    :card_file_box: Add table for engagement
    Added a table model for engagements and also created the
    relationship between a user, a blog and an engagement.

--- src/model/blog.py
+++ src/model/blog.py
@@ -1,21 +1,26 @@
+from typing import List
 from datetime import datetime
 import sqlalchemy as sa
-from sqlalchemy.orm import relation
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
 from . import SQLAlchemyBase
+from .user import User
+from .engagement import Engagement
 from src.domain_logic.blog_domain import BlogDomain
 
 
 class Blog(SQLAlchemyBase):
     __tablename__ = "blogs"
-    id: str = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     title: str = sa.Column(sa.String, nullable=False)
     body: str = sa.Column(sa.String)
     created_data: datetime = sa.Column(sa.DateTime)
     last_updated: datetime = sa.Column(sa.DateTime)
     user_id: int = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
-    user = relation("User")
+    user: User = relationship("User")
+    engagements: List[Engagement] = relationship("Engagement", backref=backref(
+        "blog", cascade="all, delete-orphan"))
 
-    
     def __init__(self, blogDomain: BlogDomain):
         self.title = blogDomain.title
         self.body = blogDomain.body
--- src/model/user.py
+++ src/model/user.py
@@ -1,6 +1,10 @@
+from typing import List
 import sqlalchemy as sa
-from sqlalchemy.orm import relation
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
 from . import SQLAlchemyBase
+from .blog import Blog
+from .engagement import Engagement
 from src.domain_logic.user_domain import UserDomain
 from src.util.security.password_util import hash_password
 
@@ -12,7 +16,9 @@ class User(SQLAlchemyBase):
                               nullable=False, index=True)
     email: str = sa.Column(sa.String, unique=True, index=True, nullable=False)
     password: str = sa.Column(sa.String, nullable=False)
-    blogs = relation("Blog", back_populates="user")
+    blogs: List[Blog] = relationship("Blog", backref=backref(
+        "user", cascade="all, delete-orphan"))
+    engagements: List[Engagement] = relationship("Engagement", backref="user")
 
     def __init__(self, userDomain: UserDomain):
         self.username = userDomain.username
