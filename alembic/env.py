cb0038f46520fa555dfbec280ae2214c08536b1c
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-06-28 20:34:24 +0100

    :hammer: Add neccessary imports in env.py
    Added all the neccessary imports and configs for creating the database
    using alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,17 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from src.model import SQLAlchemyBase
+import src.model.__all_models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +21,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLAlchemyBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
