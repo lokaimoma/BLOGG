86846a3fadd9f4d72991e40e3d227398ee7421eb
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-06-29 18:23:10 +0100

    :test_tube: Write tests for inserting engagements

---
+++ src/tests/test_insert_engagement.py
@@ -0,0 +1,51 @@
+import unittest
+from aiounittest import async_test
+from sqlalchemy import select
+from . import get_test_database_session
+from . import create_all_tables
+from . import drop_all_tables
+from src.domain_logic.user_domain import UserDomain
+from src.domain_logic.blog_domain import BlogDomain
+from src.domain_logic.engagement_domain import EngagementDomain
+from src.usecases.insert_user import insert_user
+from src.usecases.insert_blog import insert_blog
+from src.usecases.insert_engagement import insertEngagement
+from src.model.engagement import Engagement
+
+
+class InsertEngament(unittest.TestCase):
+    @async_test
+    async def test_insert_engagment(self):
+        await create_all_tables()
+        # insert user, id needed to create blog
+        user_data = {"username": "Mako",
+                     "email": "mako@mako_mail.com",
+                     "password": "Zu(|<erBerG"
+                     }
+        user_domain = UserDomain(**user_data)
+        await insert_user(userDomain=user_domain, func=get_test_database_session)
+
+        # create blog
+        # user id will always be 1
+        blog_data = {
+            "title": "Async capabilities",
+            "body": "With async we can write apis that scale",
+            "user_id": 1
+        }
+        blog_domain = BlogDomain(**blog_data)
+        await insert_blog(blogDomain=blog_domain, func=get_test_database_session)
+
+        engagement_data = {"blog_id": 1, "user_id": 1, "isLiked": True}
+        engagement_domain = EngagementDomain(**engagement_data)
+        await insertEngagement(engagementDomain=engagement_domain, func=get_test_database_session)
+
+        query = select(Engagement).filter(Engagement.id == 1)
+        session = get_test_database_session()
+
+        result = await session.execute(query)
+        engament: Engagement = result.scalar_one_or_none()
+        self.assertIsNotNone(engament)
+        self.assertTrue(engament.isLiked)
+        self.assertFalse(engament.isDisLiked)
+
+        await drop_all_tables()
--- test_suite.py
+++ test_suite.py
@@ -1,11 +1,13 @@
 import unittest
 from src.tests.test_insert_user import InsertUser
 from src.tests.test_insert_blog import InsertBlog
+from src.tests.test_insert_engagement import InsertEngament
 
 
 suite1 = unittest.TestLoader().loadTestsFromTestCase(InsertUser)
 suite2 = unittest.TestLoader().loadTestsFromTestCase(InsertBlog)
-suite_set = unittest.TestSuite(tests=[suite1, suite2, ])
+suite3 = unittest.TestLoader().loadTestsFromTestCase(InsertEngament)
+suite_set = unittest.TestSuite(tests=[suite1, suite2, suite3, ])
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(suite_set)
