f21ab56fc2ea1529858ab9917c27d92c3629383c
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-06-29 02:22:16 +0100

    Add usecase for inserting a user

---
+++ src/usecases/insert_user.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy import func
+from src.model import get_database_session
+from src.domain_logic.user_domain import UserDomain
+from src.model.user import User
+
+
+async def insert_user(userDomain: UserDomain) -> bool:
+    async with get_database_session() as session:
+        isUserRegistered = await __check_if_user_exist(session=session, email=userDomain.email, username=userDomain.username)
+
+        if not isUserRegistered:
+            user = User(userDomain=userDomain)
+            session.add(user)
+            await session.commit()
+
+        return (not isUserRegistered)
+
+
+
+async def __check_if_user_exist(session: AsyncSession,
+                          email: str, username: str) -> bool:
+    query = select(func.count(User.id)).filter(User.username == username, User.email == email)
+    result = await session.execute(query)
+    result = result.scalar()
+    return (result == 1)
