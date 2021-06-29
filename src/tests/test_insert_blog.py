c49653e857fd88b9c07c4230a3565c7a79d2b017
Owusu kelvin Clark <kelvinclark2k77@gmail.com>
2021-06-29 18:28:36 +0100

    :test_tube: Test blog data inserted into database

--- src/tests/test_insert_blog.py
+++ src/tests/test_insert_blog.py
@@ -1,4 +1,5 @@
 import unittest
+from sqlalchemy import select
 from aiounittest import async_test
 from . import get_test_database_session
 from . import create_all_tables
@@ -7,6 +8,7 @@ from src.domain_logic.user_domain import UserDomain
 from src.domain_logic.blog_domain import BlogDomain
 from src.usecases.insert_user import insert_user
 from src.usecases.insert_blog import insert_blog
+from src.model.blog import Blog
 
 
 class InsertBlog(unittest.TestCase):
@@ -31,6 +33,15 @@ class InsertBlog(unittest.TestCase):
         }
         blog_domain = BlogDomain(**blog_data)
         await insert_blog(blogDomain=blog_domain, func=get_test_database_session)
+
+        query = select(Blog).filter(Blog.id == 1)
+        session = get_test_database_session()
+        result = await session.execute(query)
+        blog: Blog = result.scalar_one_or_none()
+
+        self.assertIsNotNone(blog)
+        self.assertEqual(blog.title, "Async capabilities")
+        self.assertEqual(blog.body, "With async we can write apis that scale")
         await drop_all_tables()
 
 
