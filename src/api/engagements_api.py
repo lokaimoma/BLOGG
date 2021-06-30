373246b7ccff28cab4587dcf3e637ecc4d950231
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-06-30 20:40:19 +0100

    :globe_with_meridians: Add update engagement endpoint

--- src/api/engagements_api.py
+++ src/api/engagements_api.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 import starlette.status as StatusCode
 from src.domain_logic.engagement_domain import EngagementDomain
 from src.usecases.insert.insert_engagement import insertEngagement
-
+from ..usecases.update.update_engagement import update_engagement
 
 engagement_router = APIRouter(
     prefix=f"{prefix}/engagement", tags=["engagements"])
@@ -13,6 +13,12 @@ engagement_router = APIRouter(
 @engagement_router.post(path="/insert",
                         response_model=EngagementDomain,
                         status_code=StatusCode.HTTP_201_CREATED)
-async def insert_Engagement(engagement: EngagementDomain):
-    await insertEngagement(engagementDomain=engagement)
+async def insert(engagement: EngagementDomain):
+    await insert(engagementDomain=engagement)
+    return JSONResponse(content=engagement.__dict__, media_type="application/json")
+
+
+@engagement_router.post(path="/update", response_model=EngagementDomain, status_code=StatusCode.HTTP_201_CREATED)
+async def update(engagement: EngagementDomain):
+    await update_engagement(engagement_domain=engagement)
     return JSONResponse(content=engagement.__dict__, media_type="application/json")
