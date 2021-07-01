aaff28e07e42de2492ed7e88c62d026b0a95c5b4
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-01 12:11:05 +0100

    :building_construction: Get blog detail
    Created a function to get blog detail. I also changed the
    blog details domain to nest the blog domain instead of
    inheriting from it.

--- src/domain_logic/blog_domain_detailed.py
+++ src/domain_logic/blog_domain_detailed.py
@@ -1,8 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
 from src.domain_logic.blog_domain import BlogDomain
 
 
-class BlogDomainDetail(BlogDomain):
-    current_user_likes: bool
+class BlogDomainDetail(BaseModel):
+    blog: BlogDomain
+    current_user_likes: Optional[bool]
     likes_count: int
     dislikes_count: int
 
