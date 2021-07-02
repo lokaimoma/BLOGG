0d57cae5bb2c3755c13641b634e3b39c0d5ca537
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-02 16:56:42 +0100

    :construction: Return newest blogs first

--- src/usecases/getters/get_blogs_by_user_id.py
+++ src/usecases/getters/get_blogs_by_user_id.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, Callable
 
-from sqlalchemy import select
+from sqlalchemy import select, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.model import get_database_session
@@ -10,7 +10,7 @@ from src.model.blog import Blog
 async def get_blogs_by_user_id(user_id: int,
                                func: Callable[[], AsyncSession]
                                = get_database_session) -> Optional[List[Blog]]:
-    query = select(Blog).filter(Blog.user_id == user_id)
+    query = select(Blog).filter(Blog.user_id == user_id).order_by(desc(Blog.last_updated))
     async with func() as session:
         result = await session.execute(query)
         return result.scalars().all()
