5eba3daddd690f14973f0d2d3cb7ccef0a13bcb1
Owusu Kelvin Clark <kelvinclark2k77@gamil.com>
2021-07-03 00:57:26 +0100

    :green_heart: Fix Breaking Test

--- src/usecases/delete_blog.py
+++ src/usecases/delete_blog.py
@@ -4,11 +4,14 @@ from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.model.blog import Blog
+from src.model.engagement import Engagement
 
 
 async def delete_blog(blog_id: int,
                       func: Callable[[], AsyncSession]):
-    query = delete(Blog).where(Blog.id == blog_id)
+    blog_query = delete(Blog).where(Blog.id == blog_id)
+    engagement_query = delete(Engagement).where(Engagement.blog_id == blog_id)
     async with func() as session:
-        await session.execute(query)
+        await session.execute(blog_query)
+        await session.execute(engagement_query)
         await session.commit()
